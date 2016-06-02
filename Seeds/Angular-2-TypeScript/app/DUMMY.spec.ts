c7ec188a613236c04b143d3cc4523300216af86f
Simon Wendel <mail@simonwendel.se>
2016-06-02 16:20:03 +0200

    feat: Add import statement to dummy spec to test modules

--- Seeds/Angular-2-TypeScript/app/DUMMY.spec.ts
+++ Seeds/Angular-2-TypeScript/app/DUMMY.spec.ts
@@ -1,3 +1,7 @@
+import { DummyField } from './DUMMY';
+
 describe('TEST THAT THE RUNNER WORKS', () => {
     it('true is true', () => expect(true).toEqual(true));
+
+    it('Imported DummyField has some value', () => expect(DummyField).toEqual('Some value'));
 });
---
+++ Seeds/Angular-2-TypeScript/app/DUMMY.ts
@@ -0,0 +1,1 @@
+export const DummyField = 'Some value';
