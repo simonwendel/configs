7d846d5d319f41b67be747a19dee72f66688f635
Simon Wendel <mail@simonwendel.se>
2018-10-15 22:49:10 +0200

    feat: switch default font to Consolas

--- Files/Emacs/.emacs.d/init.el
+++ Files/Emacs/.emacs.d/init.el
@@ -19,24 +19,35 @@
 (setq global-magit-file-mode 1) ;; adds commands to file visiting buffers
 
 (custom-set-variables
- '(ansi-color-faces-vector [default default default italic underline success warning error])
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(blink-cursor-mode nil)
  '(custom-enabled-themes (quote (wombat)))
-
- ;; remove stuff in the way
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
+ '(nyan-mode t)
+ '(package-selected-packages (quote (org nyan-mode magit-svn)))
  '(tool-bar-mode nil))
 
 '(package-archives
   (quote
    (("gnu" . "https://elpa.gnu.org/packages/")
     ("melpa" . "https://melpa.org/packages/"))))
-'(package-selected-packages (quote (magit-svn magit org)))
+'(package-selected-packages (quote (nyan-mode magit-svn magit org)))
 
 ;; Visual Studio default font
 (custom-set-faces
- '(default ((t (:family "Leelawadee UI" :foundry "outline" :slant normal :weight normal :height 140 :width normal))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "outline" :family "Consolas"))))
  '(cursor ((t (:background "magenta")))))
 
 ;; flashes the modeline instead of bell for notifications
