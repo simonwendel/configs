335832b25f6645186ad70bbbedbbfb67d9d78adc
Simon Wendel <mail@simonwendel.se>
2018-10-15 20:53:02 +0200

    feat: add magit-svn and up the font a tiny bit

--- Files/Emacs/.emacs.d/init.el
+++ Files/Emacs/.emacs.d/init.el
@@ -19,30 +19,30 @@
 (setq global-magit-file-mode 1) ;; adds commands to file visiting buffers
 
 (custom-set-variables
-
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(blink-cursor-mode nil)
  '(custom-enabled-themes (quote (wombat)))
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
- '(tool-bar-mode nil)
- 
- '(package-archives
-   (quote
-    (("gnu" . "https://elpa.gnu.org/packages/")
-     ("melpa" . "https://melpa.org/packages/"))))
- '(package-selected-packages (quote (magit org))))
+ '(tool-bar-mode nil))
+
+'(package-archives
+  (quote
+   (("gnu" . "https://elpa.gnu.org/packages/")
+    ("melpa" . "https://melpa.org/packages/"))))
+'(package-selected-packages (quote (
+				    magit-svn
+				    magit
+				    org)))
 
 ;; Visual Studio default font
 (custom-set-faces
- '(default ((t (:family "Leelawadee UI" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Leelawadee UI" :foundry "outline" :slant normal :weight normal :height 140 :width normal)))))
 
 ;; flashes the modeline instead of bell for notifications
 (setq ring-bell-function
-      (lambda ()
-        (let ((orig-fg (face-foreground 'mode-line)))
-          (set-face-foreground 'mode-line "#F2804F")
-          (run-with-idle-timer 0.1 nil
-                               (lambda (fg) (set-face-foreground 'mode-line fg))
-                               orig-fg))))
+      (lambda () (let ((orig-fg (face-foreground 'mode-line)))
+		   (set-face-foreground 'mode-line "#F2804F")
+		   (run-with-idle-timer 0.1 nil
+					(lambda (fg) (set-face-foreground 'mode-line fg)) orig-fg))))
