a204ab425ffde221a7b7f847b2597e3f1e06876c
Simon Wendel <mail@simonwendel.se>
2018-10-15 19:57:24 +0200

    feat: install magit, fix some shell stuff and kill startup screen

--- Files/Emacs/.emacs.d/init.el
+++ Files/Emacs/.emacs.d/init.el
@@ -1,24 +1,38 @@
 (package-initialize)
 
+;; set shell to Git bash
+(setq explicit-shell-file-name "C:/Program Files/Git/bin/bash.exe")
+(setq shell-file-name "bash")
+(setq explicit-bash.exe-args '("--login" "-i"))
+(setenv "SHELL" shell-file-name)
+(add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
+(prefer-coding-system 'utf-8)
+
+;; magit stuff ->
+(setq vc-handled-backends nil) ;; turn off vc
+
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+(global-set-key [f1] 'shell)
+
+(setq magit-refresh-status-buffer nil) ;; should be faster ops
+(setq global-magit-file-mode 1) ;; adds commands to file visiting buffers
+
 (custom-set-variables
-;; fonts and colors and crap
+
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(blink-cursor-mode nil)
  '(custom-enabled-themes (quote (wombat)))
-
-;; maximize screen space
+ '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
  '(tool-bar-mode nil)
-
-;; set up sources 
+ 
  '(package-archives
    (quote
     (("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/"))))
-
- ;; list packages to move to new machine
- '(package-selected-packages (quote (org))))
+ '(package-selected-packages (quote (magit org))))
 
 ;; Visual Studio default font
 (custom-set-faces
