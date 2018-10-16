7ec65dd758fce85275cd01d4413cfdb7dfe880fe
Simon Wendel <mail@simonwendel.se>
2018-10-16 08:46:25 +0200

    feat: add sunrise commander to init.el
    Requires cloning github repo into local/

--- Files/Emacs/.emacs.d/init.el
+++ Files/Emacs/.emacs.d/init.el
@@ -1,5 +1,15 @@
 (package-initialize)
 
+;; sunrise commander stuff
+;; remember to clone into .emacs.d/local repo from
+;; https://github.com/escherdragon/sunrise-commander.git
+(add-to-list 'load-path "~/.emacs.d/local/sunrise-commander")
+(require 'sunrise-commander)
+(require 'sunrise-x-buttons)
+(require 'sunrise-x-modeline)
+(add-to-list 'auto-mode-alist '("\\.srvm\\'" . sr-virtual-mode))
+(global-set-key "\C-x\C-f" 'sunrise-cd)
+
 ;; set shell to Git bash
 (setq explicit-shell-file-name "C:/Program Files/Git/bin/bash.exe")
 (setq shell-file-name "bash")
@@ -7,46 +17,39 @@
 (setenv "SHELL" shell-file-name)
 (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
 (prefer-coding-system 'utf-8)
+(global-set-key [f1] 'shell)
 
 ;; magit stuff ->
 (setq vc-handled-backends nil) ;; turn off vc
-
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
-(global-set-key [f1] 'shell)
-
 (setq magit-refresh-status-buffer nil) ;; should be faster ops
 (setq global-magit-file-mode 1) ;; adds commands to file visiting buffers
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(blink-cursor-mode nil)
  '(custom-enabled-themes (quote (wombat)))
+ '(nyan-mode t)
+
  '(ido-enable-flex-matching t)
  '(ido-mode (quote both) nil (ido))
+
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
- '(nyan-mode t)
- '(package-selected-packages (quote (org nyan-mode magit-svn)))
  '(tool-bar-mode nil))
 
 '(package-archives
   (quote
    (("gnu" . "https://elpa.gnu.org/packages/")
     ("melpa" . "https://melpa.org/packages/"))))
+
 '(package-selected-packages (quote (nyan-mode magit-svn magit org)))
 
 ;; Visual Studio default font
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "outline" :family "Consolas"))))
  '(cursor ((t (:background "magenta")))))
 
