cc8d8342fa8c2d5b830e791e80fa7120cbfdd899
Simon Wendel <mail@simonwendel.se>
2018-10-17 18:18:50 +0200

    feat: convert init.el contents to org file

---
+++ Files/Emacs/.emacs.d/packages.el
@@ -0,0 +1,14 @@
+(package-initialize)
+
+(custom-set-variables    
+ '(package-selected-packages
+   (quote
+    (nyan-mode magit-svn magit org))))
+
+(custom-set-variables 
+ '(package-archives
+   (quote
+    (("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")))))
+
+(package-install-selected-packages)
